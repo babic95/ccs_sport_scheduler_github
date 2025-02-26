c3422fce8f15b23604d516317e3495c126e4b401
babic95 <babic95@gmail.com>
2025-02-26 04:33:14 +0100

    fix NewNotification.jsx

---
+++ CcsSportScheduler/CcsSportScheduler_API/ClientApp/src/components/NewNotification.jsx
@@ -0,0 +1,13 @@
+// Notifications.js
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+
+const NewNotification = ({ notifications, handleNotificationClick }) => {
+    return (
+        <Box>
+            
+        </Box>
+    );
+};
+
+export default NewNotification;
